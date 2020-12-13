b10a7596077bcf647b6f5b2f4c20cbf09822d7d2
Umair Jibran <umairjibran7@gmail.com>
2020-12-13 16:20:16 +0500

    add percentage increase and decrease

--- labassignment3/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ labassignment3/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -12,13 +12,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val calculateButton = findViewById<Button>(R.id.calculateButton)
+        val increaseButton = findViewById<Button>(R.id.increasePercentage)
+        val decreaseButton = findViewById<Button>(R.id.decreasePercentage)
         val percentageOf = findViewById<EditText>(R.id.percentageOf)
         val bill = findViewById<EditText>(R.id.bill)
         val total = findViewById<TextView>(R.id.result)
         calculateButton.setOnClickListener {
             val percentage = percentageOf.text.toString().toFloat()
             val totalBill = bill.text.toString().toFloat()
-            total.text = (percentage / 100 * totalBill).toString()
+            total.text = percentage.toString() + "% of " +  totalBill.toString() + " is " + (percentage / 100 * totalBill).toString()
+        }
+        increaseButton.setOnClickListener{
+            val percentage = percentageOf.text.toString().toFloat()
+            val totalBill = bill.text.toString().toFloat()
+            total.text = percentage.toString() + "% increase in " +  totalBill.toString() + " is " + ((percentage / 100 * totalBill) + totalBill)
+        }
+        decreaseButton.setOnClickListener{
+            val percentage = percentageOf.text.toString().toFloat()
+            val totalBill = bill.text.toString().toFloat()
+            total.text = percentage.toString() + "% decrease in " +  totalBill.toString() + " is " +(totalBill - (percentage / 100 * totalBill))
         }
     }
 }
--- labassignment3a/app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ labassignment3a/app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -12,7 +12,7 @@ import static java.lang.Float.parseFloat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button calculate;
+    Button calculate,increase,decrease;
     EditText bill, percentageOf;
     TextView result;
 
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         calculate = findViewById(R.id.calculateButton);
+        decrease = findViewById(R.id.decreasePercentage);
+        increase = findViewById(R.id.increasePercentage);
         bill = findViewById(R.id.bill);
         percentageOf = findViewById(R.id.percentageOf);
         result = findViewById(R.id.result);
@@ -30,7 +32,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 float billF = parseFloat(bill.getText().toString());
                 float percentageF = parseFloat(percentageOf.getText().toString());
-                result.setText((percentageF / 100 * billF) + "");
+                result.setText(percentageF + "% of " + billF + " is " + (percentageF / 100 * billF));
+            }
+        });
+        increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float billF = parseFloat(bill.getText().toString());
+                float percentageF = parseFloat(percentageOf.getText().toString());
+                result.setText(percentageF + "% increase in " + billF + " is " + ( billF + (percentageF / 100 * billF)));
+            }
+        });
+        decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float billF = parseFloat(bill.getText().toString());
+                float percentageF = parseFloat(percentageOf.getText().toString());
+                result.setText(percentageF + "% decrease in " + billF + " is " + ( billF - (percentageF / 100 * billF)));
             }
         });
 
